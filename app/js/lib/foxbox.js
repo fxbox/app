a131a9d97a18949d7ecd69887620484a6d744ef4
Guillaume Marty <edo999@gmail.com>
2016-03-02 17:14:07 +0000

    Implement UI for authentication

--- app/js/lib/foxbox.js
+++ app/js/lib/foxbox.js
@@ -1,4 +1,4 @@
-/* global Request, Headers */
+/* global Headers */
 
 'use strict';
 
@@ -6,6 +6,9 @@ import { Service } from 'components/fxos-mvc/dist/mvc';
 
 import FoxboxSettings from './foxbox-settings';
 import FoxboxDb from './foxbox-db';
+
+const settings = new FoxboxSettings();
+const db = new FoxboxDb();
 
 // The delay after which a request is considered failed.
 const REQUEST_TIMEOUT = 5000;
@@ -15,23 +18,38 @@ const REQUEST_TIMEOUT = 5000;
  *
  * @param {string} url The URL to send the request to.
  * @param {string} method The HTTP method (defaults to "GET").
- * @param {Object} body An object of key/value.
+ * @param {!Object} extraHeaders Any additional headers to include in the request.
+ * @param {!Object} body An object of key/value.
  * @return {Promise}
  */
-const loadJSON = function(url, method = 'GET', body = undefined) {
+const fetchJSON = function(url, method = 'GET', extraHeaders = {}, body = undefined) {
   method = method.toUpperCase();
-  if (method === 'GET' || method === 'HEAD') {
-    body = undefined;
-  }
 
-  let req = new Request(url, {
+  const req = {
     method,
     headers: new Headers({
       'Accept': 'application/json'
     }),
-    cache: 'no-store',
-    body: JSON.stringify(body)
-  });
+    cache: 'no-store'
+  };
+
+  if (method === 'POST' || method === 'PUT') {
+    req.headers.append('Content-Type', 'application/json;charset=UTF-8');
+  }
+  for (let header in extraHeaders) {
+    req.headers.append(header, extraHeaders[header]);
+  }
+  if (!extraHeaders.Authorization && !!settings.session) {
+    // The user is logged in, we authenticate the request.
+    req.headers.append('Authorization', `Bearer ${settings.session}`);
+  }
+
+  if (method === 'GET' || method === 'HEAD') {
+    body = undefined;
+  }
+  if (!body) {
+    req.body = JSON.stringify(body);
+  }
 
   // Workaround to catch network failures.
   return new Promise((resolve, reject) => {
@@ -41,7 +59,7 @@ const loadJSON = function(url, method = 'GET', body = undefined) {
       reject(new TypeError('Request timed out'));
     }, REQUEST_TIMEOUT);
 
-    fetch(req)
+    fetch(url, req)
       .then(res => {
         if (hasTimedOut) {
           return;
@@ -54,23 +72,63 @@ const loadJSON = function(url, method = 'GET', body = undefined) {
         } else {
           throw new TypeError(`The response returned a ${res.status} HTTP status code.`);
         }
+      })
+      .catch(error => {
+        clearTimeout(timeout);
+        reject(error);
       });
   });
 };
 
 export default class Foxbox extends Service {
-  constructor() {
-    super();
-    this.settings = new FoxboxSettings();
-    this.db = new FoxboxDb();
-  }
-
   init() {
-    return this.db.init();
+    return db.init();
   }
 
   get origin() {
-    return `${this.settings.scheme}://${this.settings.hostname}:${this.settings.port}`;
+    return `${settings.scheme}://${settings.hostname}:${settings.port}`;
+  }
+
+  get isLoggedIn() {
+    return !!settings.session;
+  }
+
+  /**
+   * Log in a user given her credentials.
+   *
+   * @param {string} username
+   * @param {string} password
+   * @return {Promise}
+   */
+  login(username, password) {
+    if (!username) {
+      return Promise.reject(new Error('Specify a user name.'));
+    }
+    if (!password) {
+      return Promise.reject(new Error('Specify a password.'));
+    }
+
+    const header = {
+      Authorization: `Basic ${btoa(`${username}:${password}`)}`
+    };
+    return fetchJSON(`${this.origin}/users/login`, 'POST', header)
+      .then(res => {
+        if (!res.session_token) {
+          throw(new Error('Token is missing.'));
+        }
+
+        settings.session = res.session_token;
+      });
+  }
+
+  /**
+   * Log out the user.
+   *
+   * @return {Promise} A promise that resolves once the user is logged out.
+   */
+  logout() {
+    settings.session = undefined;
+    return Promise.resolve();
   }
 
   /**
@@ -80,23 +138,23 @@ export default class Foxbox extends Service {
    */
   getServices() {
     return new Promise((resolve, reject) => {
-      loadJSON(`${this.origin}/services/list.json`)
+      fetchJSON(`${this.origin}/services/list.json`)
         .then(services => {
           // Let's remove the dummy services here.
           services = services.filter(service => service.name !== 'dummy service');
 
           const promises =
-            services.map(service => loadJSON(`http://localhost:3000/services/${service.id}/state`, 'GET'));
+            services.map(service => fetchJSON(`http://localhost:3000/services/${service.id}/state`));
           Promise.all(promises)
             .then(states => {
               services.forEach((service, id) => service.state = states[id]);
 
               // Clear the services db.
-              this.db.clearServices()
+              db.clearServices()
                 .then(() => {
                   // Populate the db with the latest services.
                   services.forEach(service => {
-                    this.db.setService(service);
+                    db.setService(service);
                   });
                 });
 
@@ -115,7 +173,7 @@ export default class Foxbox extends Service {
    */
   changeServiceState(id, state) {
     return new Promise((resolve, reject) => {
-      loadJSON(`${this.origin}/services/${id}/state`, 'PUT', state)
+      fetchJSON(`${this.origin}/services/${id}/state`, 'PUT', state)
         .then(res => {
           if (!res || !res.result || res.result !== 'success') {
             return reject(new Error(`The action couldn't be performed.`));
@@ -127,19 +185,19 @@ export default class Foxbox extends Service {
   }
 
   getTags() {
-    return this.db.getTags.apply(this.db, arguments);
+    return db.getTags.apply(db, arguments);
   }
 
   getService() {
     // Get data from the DB so we get the attributes, the state and the tags.
-    return this.db.getService.apply(this.db, arguments);
+    return db.getService.apply(db, arguments);
   }
 
   setService() {
-    return this.db.setService.apply(this.db, arguments);
+    return db.setService.apply(db, arguments);
   }
 
   setTag() {
-    return this.db.setTag.apply(this.db, arguments);
+    return db.setTag.apply(db, arguments);
   }
 }
---
+++ app/js/views/user-login.jsx
@@ -0,0 +1,59 @@
+/* global React */
+
+import Modal from 'js/views/modal';
+
+export default class UserLogin extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: '',
+      password: '',
+
+      isModalVisible: false,
+      title: '',
+      body: ''
+    };
+
+    this.foxbox = props.foxbox;
+  }
+
+  handleOnChange(state, evt) {
+    this.setState({ [state]: evt.target.value });
+  }
+
+  handleOnSubmit() {
+    this.foxbox.login(this.state.username, this.state.password)
+      .then(() => {
+        window.location.hash = '#services';
+      })
+      .catch(error => {
+        this.setState({
+          isModalVisible: true,
+          title: 'Login failed',
+          body: error.message || 'Please double check you credentials.'
+        });
+      });
+  }
+
+  dismissModal() {
+    this.setState({ isModalVisible: false });
+  }
+
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Project Link</h1>
+        </header>
+        <form className="user-login" onSubmit={this.handleOnSubmit.bind(this)}>
+          <label><span>User</span><input type="text" value={this.state.username}
+            onChange={this.handleOnChange.bind(this, 'username')}/></label>
+          <label><span>Password</span><input type="password" value={this.state.password}
+            onChange={this.handleOnChange.bind(this, 'password')}/></label>
+          <button>Log in</button>
+        </form>
+        <Modal visible={this.state.isModalVisible} title={this.state.title} body={this.state.body} dismiss={this.dismissModal.bind(this)}/>
+      </div>
+    );
+  }
+}
