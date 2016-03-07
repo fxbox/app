1a9d524afc42eb9df0af26cf6be8d1f0b85bd97d
Guillaume Marty <edo999@gmail.com>
2016-03-07 14:34:54 +0000

    Prefix the session item name in localStorage

--- app/js/lib/foxbox-settings.js
+++ app/js/lib/foxbox-settings.js
@@ -2,6 +2,9 @@
 
 import { Model } from 'components/fxos-mvc/dist/mvc';
 
+// Prefix all entries to avoid collisions.
+const PREFIX = 'foxbox-';
+
 const DEFAULT_SCHEME = 'http';
 const DEFAULT_HOSTNAME = 'localhost';
 const DEFAULT_PORT = 3000;
@@ -10,11 +13,11 @@ const REGISTRATION_SERVICE = 'http://knilxof.org:4242/ping';
 export default class FoxboxSettings extends Model {
   constructor() {
     super({
-      _scheme: localStorage.getItem('foxbox-scheme') || DEFAULT_SCHEME,
-      _hostname: localStorage.getItem('foxbox-hostname') || DEFAULT_HOSTNAME,
-      _port: localStorage.getItem('foxbox-port') || DEFAULT_PORT,
-      _session: localStorage.getItem('session'),
-      _skipDiscovery: localStorage.getItem('foxbox-skipDiscovery') === 'true'
+      _scheme: localStorage.getItem(`${PREFIX}scheme`) || DEFAULT_SCHEME,
+      _hostname: localStorage.getItem(`${PREFIX}hostname`) || DEFAULT_HOSTNAME,
+      _port: localStorage.getItem(`${PREFIX}port`) || DEFAULT_PORT,
+      _session: localStorage.getItem(`${PREFIX}session`),
+      _skipDiscovery: localStorage.getItem(`${PREFIX}skipDiscovery`) === 'true'
     });
   }
 
@@ -25,7 +28,7 @@ export default class FoxboxSettings extends Model {
   set scheme(scheme) {
     scheme = String(scheme) || DEFAULT_SCHEME;
     this._scheme = scheme;
-    localStorage.setItem('foxbox-scheme', this._scheme);
+    localStorage.setItem(`${PREFIX}scheme`, this._scheme);
   }
 
   get hostname() {
@@ -35,7 +38,7 @@ export default class FoxboxSettings extends Model {
   set hostname(hostname) {
     hostname = String(hostname) || DEFAULT_HOSTNAME;
     this._hostname = hostname.replace(/\/$/, ''); // Trailing slash.
-    localStorage.setItem('foxbox-hostname', this._hostname);
+    localStorage.setItem(`${PREFIX}hostname`, this._hostname);
   }
 
   get port() {
@@ -45,7 +48,7 @@ export default class FoxboxSettings extends Model {
   set port(port) {
     port = parseInt(port, 10) || DEFAULT_PORT;
     this._port = port;
-    localStorage.setItem('foxbox-port', this._port);
+    localStorage.setItem(`${PREFIX}port`, this._port);
   }
 
   get session() {
@@ -55,10 +58,10 @@ export default class FoxboxSettings extends Model {
   set session(session) {
     if (session === undefined) {
       this._session = undefined;
-      localStorage.removeItem('session');
+      localStorage.removeItem(`${PREFIX}session`);
     } else {
       this._session = session;
-      localStorage.setItem('session', this._session);
+      localStorage.setItem(`${PREFIX}session`, this._session);
     }
   }
 
@@ -73,6 +76,6 @@ export default class FoxboxSettings extends Model {
   set skipDiscovery(value) {
     value = !!value;
     this._skipDiscovery = value;
-    localStorage.setItem('foxbox-skipDiscovery', value);
+    localStorage.setItem(`${PREFIX}skipDiscovery`, value);
   }
 }
