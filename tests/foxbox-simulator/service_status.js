5cd0738780f7c2fd8ec484b931ae960f51712fe4
isabelrios <isabelrios@gmail.com>
2016-03-22 16:27:59 +0100

    more fixes

--- tests/foxbox-simulator/service_status.js
+++ tests/foxbox-simulator/service_status.js
@@ -12,35 +12,40 @@ var light_status;
 service.use(bodyParser.json());
 
 function turnOnLight(on) {
-	console.log('On');
-	light_status = true;
+  console.log('On');
+  light_status = true;
 }
 
 function turnOffLight(on) {
-	console.log('Off');
-	light_status = false;
+  console.log('Off');
+  light_status = false;
 }
 
 service.route('/')
 .all(function(req,res,next) {
-      res.writeHead(200, { 'Content-Type': 'application/json' });
+      //res.writeHead(200, { 'Content-Type': 'application/json' });
+      req.headers['content-type'] = 'application/json';
       next();
 })
 
 .get(function(req,res,next){
-	var readable = fs.createReadStream(state);
+  var readable = fs.createReadStream(state);
        readable.pipe(res);
-
+       res.status(200);
 })
+
 .put(function(req, res, next){
-   if (req.body.on) {
+   if (req.body.on === true) {
           console.log(req.body.on + ' ' + 'so turn off');
           turnOffLight(req.params.on);
         }
-        else {
+          else {
           console.log(req.body.on + ' ' + 'so turn on');  
           turnOnLight(req.params.on);    
     }
+
+    var response = {"response":"success"};
+    res.status(200).json(response);
 });
 
 app.use('/services/:serviceId/state', service);
