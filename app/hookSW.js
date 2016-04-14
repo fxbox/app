415a467cfeb20e3996e590ceb26e671ccf24173f
Francisco Jordano <fjordano@mozilla.com>
2016-04-14 19:02:07 +0100

    Notifying via postmessage all the opened windows and focusing in the tabs when notification is clicked.

--- app/hookSW.js
+++ app/hookSW.js
@@ -15,10 +15,13 @@ self.addEventListener('push', function(evt) {
   console.log('Push received ', JSON.stringify(evt.data));
   var obj = evt.data ? evt.data.json() : {};
 
-  if (obj && obj.body) {
-    showNotification(obj, evt);
+  if (obj && obj.message) {
+    notifyClient(obj)
+      .then(() => {
+        showNotification(obj, evt);
+      });
   } else {
-    console.log('Discarding notification');
+    console.log('Discarding notification: ' + JSON.stringify(obj));
   }
 });
 
@@ -26,13 +29,51 @@ self.addEventListener('pushsubscriptionchange', function(evt) {
   console.log('Got subscription change event ', evt);
 });
 
+self.addEventListener('notificationclick', function(evt) {
+  evt.notification.close();
+  event.waitUntil(clients.matchAll({
+    type: "window"
+  }).then(function(clients) {
+    clients.forEach((client) => {
+      if ('focus' in client) {
+        client.focus();
+      }
+    });
+  }));
+});
+
+/**
+ * Notify the foxlink library of the message received via push.
+ * This will notify all the windows/tabs opened with the app.
+ * @param {object} obj Payload coming from the push notification
+ */
+function notifyClient(obj) {
+  return self.clients.matchAll()
+    .then((clients) => {
+      clients.forEach((client) => {
+        var senderID = event.source ? event.source.id : 'unknown';
+        client.postMessage({
+          client: senderID,
+          message: obj
+        });
+      });
+    });
+}
+
+/**
+ * Displays a notification based on the data coming from the
+ * push.
+ * @param {object} obj Payload coming from the push notification
+ * @param {event} evt Original event generated
+ * @return {Promise} Promise resolved once the notification is showed
+ */
 function showNotification(obj, evt) {
   var title = 'Link: Notification';
-  var body = obj.body;
+  var body = obj.message;
   var icon = 'img/icon.svg';
-  var tag = obj.tag || 'link-push';
+  var tag = obj.resource || obj.tag || 'link-push';
 
-  evt.waitUntil(self.registration.showNotification(title, {
+  return evt.waitUntil(self.registration.showNotification(title, {
     body: body,
     icon: icon,
     tag: tag
