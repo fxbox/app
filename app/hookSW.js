f9a3105e53ffd7c47b16287bc250830501da2177
Francisco Jordano <arcturus@ardeenelinfierno.com>
2016-04-15 10:52:25 +0100

    Webpush click and notificating via postMessage (#88)
    Notifying via postmessage all the opened windows and focusing in the tabs when notification is clicked.

--- app/hookSW.js
+++ app/hookSW.js
@@ -15,10 +15,16 @@ self.addEventListener('push', function(evt) {
   console.log('Push received ', JSON.stringify(evt.data));
   var obj = evt.data ? evt.data.json() : {};
 
-  if (obj && obj.body) {
-    showNotification(obj, evt);
+  if (obj && obj.message) {
+    notifyClient(obj)
+      .then(() => {
+        showNotification(obj, evt);
+      })
+      .catch((error) => {
+        console.error('Error processing notification: ', error);
+      });
   } else {
-    console.log('Discarding notification');
+    console.error('Notification doesnt contain a body, ignoring it: ');
   }
 });
 
@@ -26,13 +32,50 @@ self.addEventListener('pushsubscriptionchange', function(evt) {
   console.log('Got subscription change event ', evt);
 });
 
+self.addEventListener('notificationclick', function(evt) {
+  evt.notification.close();
+  evt.waitUntil(clients.matchAll({
+    type: "window"
+  })
+  .then(function(clients) {
+    clients.forEach((client) => {
+      if ('focus' in client) {
+        client.focus();
+      }
+    });
+  }));
+});
+
+/**
+ * Notify the foxlink library of the message received via push.
+ * This will notify all the windows/tabs opened with the app.
+ *
+ * @param {Object} obj Payload coming from the push notification
+ */
+function notifyClient(obj) {
+  return self.clients.matchAll()
+    .then((clients) => {
+      clients.forEach((client) => {
+        client.postMessage(obj);
+      });
+    });
+}
+
+/**
+ * Displays a notification based on the data coming from the
+ * push.
+ *
+ * @param {object} obj Payload coming from the push notification
+ * @param {event} evt Original event generated
+ * @return {Promise} Promise resolved once the notification is showed
+ */
 function showNotification(obj, evt) {
   var title = 'Link: Notification';
-  var body = obj.body;
+  var body = obj.message;
   var icon = 'img/icon.svg';
-  var tag = obj.tag || 'link-push';
+  var tag = obj.resource || obj.tag || 'link-push';
 
-  evt.waitUntil(self.registration.showNotification(title, {
+  return evt.waitUntil(self.registration.showNotification(title, {
     body: body,
     icon: icon,
     tag: tag
