1036514aaee6d989b49f0103a8777c2ab6814d81
Guillaume Marty <edo999@gmail.com>
2016-06-13 12:31:30 +0100

    Deployment is based on fda21f4

--- sw.js
+++ sw.js
@@ -1,14 +1,1 @@
-importScripts('files.js');
-  importScripts('sww.js');
-
-  var version = '1.0.0';
-  var worker = new self.ServiceWorkerWare();
-  worker.use(new self.StaticCacher(FILES_TO_LOAD));
-  worker.use(new self.SimpleOfflineCache());
-
-  var extraFile = 'hookSW.js';
-  if (extraFile && extraFile !== 'null') {
-    importScripts(extraFile);
-  }
-
-  worker.init();
+importScripts("files.js");importScripts("sww.js");var version="1.0.0",worker=new self.ServiceWorkerWare;worker.use(new self.StaticCacher(FILES_TO_LOAD));worker.use(new self.SimpleOfflineCache);var extraFile="hookSW.js";extraFile&&"null"!==extraFile&&importScripts(extraFile);worker.init()
